894072bb878e10c25cb349bbae69614dc020adc3
张绎凡 <zhangyifan@zhangyifandeMacBook-Pro.local>
2021-12-02 22:40:24 +0800

    first commit

---
+++ src/main/java/com/zyf/ZyfApplication.java
@@ -0,0 +1,13 @@
+package com.zyf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyfApplication.class, args);
+    }
+
+}
