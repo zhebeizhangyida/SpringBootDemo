d5710ece706a9a49a2d799be5d617a2c754dc56e
zhangyifan <747201276@qq.com>
2021-12-07 10:46:57 +0800

    集成druid

--- src/main/java/com/zyf/ZyfApplication.java
+++ src/main/java/com/zyf/ZyfApplication.java
@@ -3,9 +3,10 @@ package com.zyf;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("com.zyf.dao")
 @EnableCaching //开启redis缓存
 public class ZyfApplication {
